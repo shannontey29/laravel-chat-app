7ed2e12fae51db51e07f6e3cd5028e1d103b8ada
shannontey29 <shannontey29@gmail.com>
2025-08-22 22:38:19 +0800

    feat(chat): add edit message functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Helpers\Helper;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         \Broadcast::channel('online-users', function ($user) {
             return [
                 'id' => $user->id,
