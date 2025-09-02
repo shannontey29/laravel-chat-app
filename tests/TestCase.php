584daab7e85c008a120f75c3b7d795f07ddc8a2f
shannontey29 <shannontey29@gmail.com>
2025-09-02 14:15:08 +0800

    CI Workflow

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Notification;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Fake notifications to prevent real email sending
+        Notification::fake();
+    }
 }
