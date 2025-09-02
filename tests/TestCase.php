bc8eff6ba0cf9d9e283cb437d8fa157a5194ae8f
shannontey29 <shannontey29@gmail.com>
2025-09-02 12:22:57 +0800

    Add Laravel + React CI workflow with automated tests and reports

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,17 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        config(['hashing.driver' => 'bcrypt']);
-        Hash::driver('bcrypt');
-    }
 }
